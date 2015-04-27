385d5be0f8874b8c22580a2575e090b533ab3a4f
Fabpk90 <fabrizio1996@live.fr>
2015-04-27 17:13:11 +0200

    creating a tp object

---
+++ src/utils/PartyHard_Tp.java
@@ -0,0 +1,57 @@
+package utils;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PartyHard_Tp {
+
+	public Vector2 position;
+	public Vector2 destination;
+
+	public String NameMap;
+	
+	public PartyHard_Tp(float xPos, float yPos, String nameNewMap, float xDestination, float yDestination) {
+		
+		position.x = xPos;
+		position.y = yPos;
+		
+		NameMap = nameNewMap;
+		destination.x = xDestination;
+		destination.y = yDestination;		
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+	public void setPosition(float x, float y)
+	{
+		position.x = x;
+		position.y = y;
+	}
+
+	public String getNameMap() {
+		return NameMap;
+	}
+
+	public void setNameMap(String nameMap) {
+		NameMap = nameMap;
+	}
+	
+	public Vector2 getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Vector2 destination) {
+		this.destination = destination;
+	}
+	
+	public void setDestination(float x, float y)
+	{
+		destination.x = x;
+		destination.y = y;
+	}
+
+}
